12b7063c872be93826cb3fe9bcce5f2698129746
Yevhenii <anahielitl@gmail.com>
2023-11-08 22:37:26 +0200

    HMW v.0.1

---
+++ Task_1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RichTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Проверяем, есть ли текст, который является водяным знаком
+            if (placeholderText.Text == "Введите текст здесь...")
+            {
+                // Очищаем RichTextBox
+                richTextBoxWithPlaceholder.Document.Blocks.Clear();
+            }
+        }
+
+        private void RichTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Проверяем, пуст ли RichTextBox
+            if (!richTextBoxWithPlaceholder.Document.Blocks.OfType<Paragraph>().Any(p => p.Inlines.Any()))
+            {
+                // Вставляем водяной знак
+                FlowDocument document = new FlowDocument();
+                Paragraph paragraph = new Paragraph();
+                Run run = new Run("Введите текст здесь...")
+                {
+                    Foreground = Brushes.Gray,
+                    FontStyle = FontStyles.Italic
+                };
+                paragraph.Inlines.Add(run);
+                document.Blocks.Add(paragraph);
+                richTextBoxWithPlaceholder.Document = document;
+            }
+        }
+    }
+}
--- Task_1/Properties/AssemblyInfo.cs
+++ Task_1/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Task#1")]
+[assembly: AssemblyTitle("Task_1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task#1")]
+[assembly: AssemblyProduct("Task_1")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
