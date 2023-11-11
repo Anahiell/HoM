6827fa927f2161c21bbd0a4d2c7baea631474738
Бурдейный Евгений <anahielitl@gmail.com>
2023-11-11 20:32:31 +0200

    home work v.1

--- HMW_TASKs/Program.cs
+++ HMW_TASKs/Program.cs
@@ -4,33 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.IO;
 
 namespace HMW_TASKs
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
             Console.WriteLine("chouse the task:");
             int x = int.Parse(Console.ReadLine());
-            switch (x)
+            while (x != 0)
             {
-                case 1:
-                    {
-                        string path = @"C:\Users\Monkiell\OneDrive\Рабочий стол\системное прогр\HoM\Task_1\bin\Debug\Task_1.exe";
-                        Process task1 = new Process();
-                        task1.StartInfo.FileName = path;
-                        task1.Start();
-                        break;
-                    }
-                case 2:
-                    {
+                switch (x)
+                {
+                    case 1:
+                        {
+                            //задание 1-2
+                            string path = @"C:\Users\Monkiell\OneDrive\Рабочий стол\системное прогр\HoM\Task_1\bin\Debug\Task_1.exe";
+                            Process task1 = new Process();
+                            task1.StartInfo.FileName = path;
+                            task1.Start();
+                            task1.WaitForExit();
+                            break;
+                        }
+                    case 2:
 
+                        {
+
+                            string pathToProject3 = @"C:\Users\Monkiell\OneDrive\Рабочий стол\системное прогр\HoM\Task_3\bin\Debug\Task_3.exe";
+                            string pathToProject4 = @"C:\Users\Monkiell\OneDrive\Рабочий стол\системное прогр\HoM\Task_4\bin\Debug\Task_4.exe";
+                            string pathToProject5 = @"C:\Users\Monkiell\OneDrive\Рабочий стол\системное прогр\HoM\Task_5\bin\Debug\Task_5.exe";
+                            string pathToProject6 = @"C:\Users\Monkiell\OneDrive\Рабочий стол\системное прогр\HoM\Task_6\bin\Debug\Task_6.exe";
+
+
+                            var tasks = new List<Task>
+                         {
+                             Task.Run(() => { Process.Start(pathToProject3); }),
+                            Task.Run(() => { Process.Start(pathToProject4); }),
+                            Task.Run(() => { Process.Start(pathToProject5); }),
+                             Task.Run(() => { Process.Start(pathToProject6); })
+                         };
+
+                            // wait all tasks
+
+                            await Task.WhenAll(tasks);
+
+                            break;
+                        }
+
+                    default:
                         break;
-                    }
-                default:
-                    break;
+                }
+                Console.WriteLine("chouse the task:");
+                 x = int.Parse(Console.ReadLine());
             }
         }
+
     }
 }
---
+++ Task_4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // some num
+            int number = 12345;
+
+            var factorialTask = Task.Run(() => CalculateFactorial(number));
+            var digitCountTask = Task.Run(() => CountDigits(number));
+            var digitSumTask = Task.Run(() => SumDigits(number));
+
+            // wait all 
+            await Task.WhenAll(factorialTask, digitCountTask, digitSumTask);
+
+           
+            Console.WriteLine($"Number: {number}");
+            Console.WriteLine($"Factorial: {factorialTask.Result}");
+            Console.WriteLine($"Count of cifri: {digitCountTask.Result}");
+            Console.WriteLine($"Summ cifri: {digitSumTask.Result}");
+            Console.ReadKey();
+        }
+
+        static int CalculateFactorial(int n)
+        {
+            return ParallelEnumerable.Range(1, n).Aggregate(1, (acc, i) => acc * i);
+        }
+
+        static int CountDigits(int number)
+        {
+            return ParallelEnumerable.Range(1, number.ToString().Length).Count();
+        }
+
+        static int SumDigits(int number)
+        {
+            object lockObject = new object();
+            int sum = 0;
+
+            Parallel.ForEach(number.ToString(), digit =>
+            {
+                int digitValue = int.Parse(digit.ToString());
+
+                lock (lockObject)
+                {
+                    sum += digitValue;
+                }
+            });
+
+            return sum;
+        }
+    }
+}
